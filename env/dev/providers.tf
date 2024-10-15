7c97e0bd1d532e5fa1ae7545e9285c09afc14519
CristianRC03 <crizcamach2003@gmail.com>
2024-10-14 19:25:53 -0600

    Apply

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name = "NetworkWatcherRG"
+    storage_account_name = "711cristian"
+    container_name = "711-74576-tfstate"
+    key = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
